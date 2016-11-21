f4e4930d1d30e769c739107d974bad2d9a32f79b
Pead Nong <anuchit.prasertsang@gmail.com>
2016-11-21 22:35:43 +0700

    clear database before run test

--- silk_test.go
+++ silk_test.go
@@ -4,10 +4,22 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+
 	"github.com/matryer/silk/runner"
 )
 
 func TestOrdersEndpoint(t *testing.T) {
+	session, err := mgo.Dial("mongodb://localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	session.SetMode(mgo.Monotonic, true)
+	c := session.DB("qorders").C("orders")
+	c.RemoveAll(bson.M{})
 	s := httptest.NewServer(newAPI().MakeHandler())
 	defer s.Close()
 
